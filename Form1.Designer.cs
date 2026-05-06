1a4aa492c0889ccb23aa7ea11fb9d6ec251ab6c0
Kimariv <naggets394@gmail.com>
2026-05-06 10:16:47 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace Практичкеская_работа__3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxА1 = new System.Windows.Forms.TextBox();
+            this.textBoxА2 = new System.Windows.Forms.TextBox();
+            this.textBoxА3 = new System.Windows.Forms.TextBox();
+            this.textBoxА4 = new System.Windows.Forms.TextBox();
+            this.textBoxА5 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выполнитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupListA = new System.Windows.Forms.GroupBox();
+            this.groupBoxRandom = new System.Windows.Forms.GroupBox();
+            this.radioBtnRandTrue = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxA6 = new System.Windows.Forms.TextBox();
+            this.textBoxA7 = new System.Windows.Forms.TextBox();
+            this.textBoxA8 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonOn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupListA.SuspendLayout();
+            this.groupBoxRandom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxА1
+            // 
+            this.textBoxА1.Location = new System.Drawing.Point(18, 29);
+            this.textBoxА1.Name = "textBoxА1";
+            this.textBoxА1.Size = new System.Drawing.Size(222, 20);
+            this.textBoxА1.TabIndex = 0;
+            // 
+            // textBoxА2
+            // 
+            this.textBoxА2.Location = new System.Drawing.Point(18, 67);
+            this.textBoxА2.Name = "textBoxА2";
+            this.textBoxА2.Size = new System.Drawing.Size(222, 20);
+            this.textBoxА2.TabIndex = 1;
+            // 
+            // textBoxА3
+            // 
+            this.textBoxА3.Location = new System.Drawing.Point(18, 108);
+            this.textBoxА3.Name = "textBoxА3";
+            this.textBoxА3.Size = new System.Drawing.Size(222, 20);
+            this.textBoxА3.TabIndex = 2;
+            // 
+            // textBoxА4
+            // 
+            this.textBoxА4.Location = new System.Drawing.Point(18, 150);
+            this.textBoxА4.Name = "textBoxА4";
+            this.textBoxА4.Size = new System.Drawing.Size(222, 20);
+            this.textBoxА4.TabIndex = 3;
+            this.textBoxА4.TextChanged += new System.EventHandler(this.textBoxА4_TextChanged);
+            // 
+            // textBoxА5
+            // 
+            this.textBoxА5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxА5.Location = new System.Drawing.Point(18, 192);
+            this.textBoxА5.Name = "textBoxА5";
+            this.textBoxА5.Size = new System.Drawing.Size(222, 20);
+            this.textBoxА5.TabIndex = 4;
+            this.textBoxА5.TextChanged += new System.EventHandler(this.textBoxА5_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(812, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выполнитьToolStripMenuItem,
+            this.очиститьToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem});
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // выполнитьToolStripMenuItem
+            // 
+            this.выполнитьToolStripMenuItem.Name = "выполнитьToolStripMenuItem";
+            this.выполнитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выполнитьToolStripMenuItem.Text = "Выполнить";
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // groupListA
+            // 
+            this.groupListA.BackColor = System.Drawing.Color.Transparent;
+            this.groupListA.Controls.Add(this.textBoxA8);
+            this.groupListA.Controls.Add(this.textBoxA7);
+            this.groupListA.Controls.Add(this.textBoxA6);
+            this.groupListA.Controls.Add(this.textBoxА5);
+            this.groupListA.Controls.Add(this.textBoxА4);
+            this.groupListA.Controls.Add(this.textBoxА3);
+            this.groupListA.Controls.Add(this.textBoxА2);
+            this.groupListA.Controls.Add(this.textBoxА1);
+            this.groupListA.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupListA.ForeColor = System.Drawing.Color.DarkBlue;
+            this.groupListA.Location = new System.Drawing.Point(115, 53);
+            this.groupListA.Name = "groupListA";
+            this.groupListA.Size = new System.Drawing.Size(259, 351);
+            this.groupListA.TabIndex = 11;
+            this.groupListA.TabStop = false;
+            this.groupListA.Text = "Первый список покупок";
+            // 
+            // groupBoxRandom
+            // 
+            this.groupBoxRandom.BackColor = System.Drawing.Color.Transparent;
+            this.groupBoxRandom.Controls.Add(this.button2);
+            this.groupBoxRandom.Controls.Add(this.button1);
+            this.groupBoxRandom.Controls.Add(this.radioBtnRandTrue);
+            this.groupBoxRandom.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBoxRandom.ForeColor = System.Drawing.Color.DarkBlue;
+            this.groupBoxRandom.Location = new System.Drawing.Point(473, 53);
+            this.groupBoxRandom.Name = "groupBoxRandom";
+            this.groupBoxRandom.Size = new System.Drawing.Size(243, 202);
+            this.groupBoxRandom.TabIndex = 13;
+            this.groupBoxRandom.TabStop = false;
+            this.groupBoxRandom.Text = "Случайное заполнение";
+            this.groupBoxRandom.Enter += new System.EventHandler(this.groupBoxRandom_Enter);
+            // 
+            // radioBtnRandTrue
+            // 
+            this.radioBtnRandTrue.AllowDrop = true;
+            this.radioBtnRandTrue.AutoSize = true;
+            this.radioBtnRandTrue.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioBtnRandTrue.Location = new System.Drawing.Point(26, 43);
+            this.radioBtnRandTrue.Name = "radioBtnRandTrue";
+            this.radioBtnRandTrue.Size = new System.Drawing.Size(192, 17);
+            this.radioBtnRandTrue.TabIndex = 0;
+            this.radioBtnRandTrue.Text = "Включить случайное заполнение";
+            this.radioBtnRandTrue.UseVisualStyleBackColor = true;
+            this.radioBtnRandTrue.CheckedChanged += new System.EventHandler(this.radioBtnRandTrue_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(35, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сгенерировать список продуктов";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(35, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Сгенерировать список продовольственных товаров";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxA6
+            // 
+            this.textBoxA6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxA6.Location = new System.Drawing.Point(18, 234);
+            this.textBoxA6.Name = "textBoxA6";
+            this.textBoxA6.Size = new System.Drawing.Size(222, 20);
+            this.textBoxA6.TabIndex = 5;
+            // 
+            // textBoxA7
+            // 
+            this.textBoxA7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxA7.Location = new System.Drawing.Point(18, 276);
+            this.textBoxA7.Name = "textBoxA7";
+            this.textBoxA7.Size = new System.Drawing.Size(222, 20);
+            this.textBoxA7.TabIndex = 6;
+            // 
+            // textBoxA8
+            // 
+            this.textBoxA8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxA8.Location = new System.Drawing.Point(18, 319);
+            this.textBoxA8.Name = "textBoxA8";
+            this.textBoxA8.Size = new System.Drawing.Size(222, 20);
+            this.textBoxA8.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(461, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Наиболее часто встречающееся слово в списке:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.BackColor = System.Drawing.Color.Transparent;
+            this.ClearBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ClearBtn.ForeColor = System.Drawing.Color.DarkBlue;
+            this.ClearBtn.Location = new System.Drawing.Point(473, 437);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(119, 19);
+            this.ClearBtn.TabIndex = 15;
+            this.ClearBtn.Text = "Очистить";
+            this.ClearBtn.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(464, 350);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // buttonOn
+            // 
+            this.buttonOn.BackColor = System.Drawing.Color.Transparent;
+            this.buttonOn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonOn.ForeColor = System.Drawing.Color.DarkBlue;
+            this.buttonOn.Location = new System.Drawing.Point(236, 437);
+            this.buttonOn.Name = "buttonOn";
+            this.buttonOn.Size = new System.Drawing.Size(119, 19);
+            this.buttonOn.TabIndex = 17;
+            this.buttonOn.Text = "Выполнить";
+            this.buttonOn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Практичкеская_работа__3.Properties.Resources.notebook_paper_background_grid_note_document_vector;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(812, 500);
+            this.Controls.Add(this.buttonOn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBoxRandom);
+            this.Controls.Add(this.groupListA);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupListA.ResumeLayout(false);
+            this.groupListA.PerformLayout();
+            this.groupBoxRandom.ResumeLayout(false);
+            this.groupBoxRandom.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxА1;
+        private System.Windows.Forms.TextBox textBoxА2;
+        private System.Windows.Forms.TextBox textBoxА3;
+        private System.Windows.Forms.TextBox textBoxА4;
+        private System.Windows.Forms.TextBox textBoxА5;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выполнитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupListA;
+        private System.Windows.Forms.GroupBox groupBoxRandom;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioBtnRandTrue;
+        private System.Windows.Forms.TextBox textBoxA8;
+        private System.Windows.Forms.TextBox textBoxA7;
+        private System.Windows.Forms.TextBox textBoxA6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonOn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практичкеская_работа__3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioBtnRandTrue_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioBtnRandTrue.Checked == true) radioBtnRandTrue.Checked = false;
+            else radioBtnRandTrue.Checked = true;
+        }
+
+        private void textBoxА5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxА4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBoxRandom_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
